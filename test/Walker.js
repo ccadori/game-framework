b492033d9f8fb618f05848ca5526e17e8ead1a7b
ccadori <carloscadori.dev@gmail.com>
2016-11-06 21:34:32 -0200

    rendering with scale and pivot

--- test/Walker.js
+++ test/Walker.js
@@ -8,16 +8,16 @@ class Walker extends Behavior
 
     update(deltaTime)
     {
-        if (Input.isPressed(Input.keyCode.downArrow))
+        if (Input.isPressed(KeyCode.downarrow))
             this.object.position.move(0,this.velocity * deltaTime)
         
-        if (Input.isPressed(Input.keyCode.upArrow))
+        if (Input.isPressed(KeyCode.uparrow))
             this.object.position.move(0,-this.velocity * deltaTime)
 
-        if (Input.isPressed(Input.keyCode.rightArrow))
+        if (Input.isPressed(KeyCode.rightarrow))
             this.object.position.move(this.velocity * deltaTime,0)
 
-        if (Input.isPressed(Input.keyCode.leftArrow))
+        if (Input.isPressed(KeyCode.leftarrow))
             this.object.position.move(-this.velocity * deltaTime,0)
 
         Camera.position.moveTo(this.object.position.x, this.object.position.y)
