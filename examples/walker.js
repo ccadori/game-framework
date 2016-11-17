8db5bfc2887b9c7083ed0c2d32db0d97b12bb4c9
ccadori <carloscadori.dev@gmail.com>
2016-11-17 18:05:59 -0200

    rotating walk example

--- examples/walker.js
+++ examples/walker.js
@@ -1,27 +1,29 @@
-class Walker extends Component
-{
-    constructor(velocity, camera)
-    {
+class Walker extends Component {
+    constructor(velocity, camera) {
         super()
         this.velocity = velocity
         this.camera = camera
     }
 
-    update(deltaTime)
-    {
-        if (Input.isPressed(KeyCode.downarrow))
+    update(deltaTime) {
+        //Movementation
+        if (Input.isPressed(KeyCode.downarrow)) {
             this.object.body.position.y += this.velocity * deltaTime
-        
-        if (Input.isPressed(KeyCode.uparrow))
+            this.object.body.rotation = 0
+        }
+        else if (Input.isPressed(KeyCode.uparrow)) {
             this.object.body.position.y -= this.velocity * deltaTime
-
-        if (Input.isPressed(KeyCode.rightarrow))
-            this.object.body.position.x += this.velocity * deltaTime
-
-        if (Input.isPressed(KeyCode.leftarrow))
+            this.object.body.rotation = 180
+        } else if (Input.isPressed(KeyCode.leftarrow)) {
             this.object.body.position.x -= this.velocity * deltaTime
+            this.object.body.rotation = 90
+        } else if (Input.isPressed(KeyCode.rightarrow)) {
+            this.object.body.position.x += this.velocity * deltaTime
+            this.object.body.rotation = 270
+        }
 
-        this.camera.body.position.x = this.object.body.position.x 
+        // Camera Follow 
+        this.camera.body.position.x = this.object.body.position.x
         this.camera.body.position.y = this.object.body.position.y
     }
 }
