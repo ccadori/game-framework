b55449f561c1189b5c6d7c55fd847dc44bce74bb
ccadori <carloscadori.dev@gmail.com>
2016-11-17 08:55:02 -0200

    new folders

---
+++ tests/core/Game.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+
+describe('Game', function(){
+    describe('new game', function(){
+        it('should start the game', function(){
+            assert.equal(1)
+        })
+    })
+})
